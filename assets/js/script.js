399ba37edc9987c7d28b8ae6f54a293c7c62045f
jpd61 <jpd@dewoodyoil.com>
2020-04-25 14:08:40 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,10 @@ var handleErrors = (response) => {
 
 // Function to get and display the current conditions on Open Weather Maps
 var getCurrentConditions = (event) => {
+    // Obtain city name from the search box
     let city = $('#search-city').val();
     currentCity= $('#search-city').val();
+    // Set the queryURL to fetch from API - added units=imperial to fix
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
     fetch(queryURL)
     .then(handleErrors)
@@ -22,16 +24,23 @@ var getCurrentConditions = (event) => {
         return response.json();
     })
     .then((response) => {
+        // Save city to local storage
         saveCity(city);
         $('#search-error').text("");
+        // Create icon for the current weather using Open Weather Maps
         let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        // Offset UTC timezone - using moment.js
         let currentTimeUTC = response.dt;
         let currentTimeZoneOffset = response.timezone;
         let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
         let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+        // Render cities list
         renderCities();
+        // Obtain the 5day forecast for the searched city
         getFiveDayForecast(event);
+        // Set the header text to the found city name
         $('#header-text').text(response.name);
+        // HTML for the results of search
         let currentWeatherHTML = `
             <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
             <ul class="list-unstyled">
@@ -40,11 +49,15 @@ var getCurrentConditions = (event) => {
                 <li>Wind Speed: ${response.wind.speed} mph</li>
                 <li id="uvIndex">UV Index:</li>
             </ul>`;
+        // Append the results to the DOM
         $('#current-weather').html(currentWeatherHTML);
+        // Get the latitude and longitude for the UV search from Open Weather Maps API
         let latitude = response.coord.lat;
         let longitude = response.coord.lon;
         let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
+        // API solution for Cross-origin resource sharing (CORS) error: https://cors-anywhere.herokuapp.com/
         uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+        // Fetch the UV information and build the color display for the UV index
         fetch(uvQueryURL)
         .then(handleErrors)
         .then((response) => {
