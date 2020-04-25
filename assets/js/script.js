8e78026961705289ee9509ef998d607181421c4e
jpd61 <jpd@dewoodyoil.com>
2020-04-25 13:55:32 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,6 @@ var owmAPI = "788d5638d7c8e354a162d6c9747d1bdf";
 var currentCity = "";
 var lastCity = "";
 
-var getURLParams = () => {
-    let urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('key')) {
-        owmAPI = urlParams.get('key');
-    }
-}
 
 var handleErrors = (response) => {
     if (!response.ok) {
@@ -176,7 +170,6 @@ var createEventListeners = () => {
 }
 
 var mainApp = () => {
-    getURLParams();
     renderCities();
     getCurrentConditions();
     createEventListeners();
