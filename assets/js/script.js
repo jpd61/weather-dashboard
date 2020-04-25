e118ae96d6a826dd70e91c0b3ffec57650e82186
jpd61 <jpd@dewoodyoil.com>
2020-04-25 14:16:22 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,19 +154,24 @@ var renderCities = () => {
         lastCity=localStorage.getItem(lastCityKey);
         // Set search input to last city searched
         $('#search-city').attr("value", lastCity);
+        // Append stored cities to page
         for (let i = 0; i < localStorage.length; i++) {
             let city = localStorage.getItem("cities" + i);
             let cityEl;
+            // Set to lastCity if currentCity not set
             if (currentCity===""){
                 currentCity=lastCity;
             }
+            // Set button class to active for currentCity
             if (city === currentCity) {
                 cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
             } else {
                 cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
             } 
+            // Append city to page
             $('#city-results').prepend(cityEl);
         }
+        // Add a "clear" button to page if there is a cities list
         if (localStorage.length>0){
             $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
         } else {
